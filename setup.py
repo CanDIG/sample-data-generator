c595c1e9b37c17238514cf8746fa300ab5bd492f
Samarth Agarwal <agarwalsamarth@hotmail.com>
2020-07-21 17:50:14 -0400

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+with open('requirements.txt', 'r') as file:
+    install_requires = file.readlines()
+
+    for i in range(len(install_requires)):
+        install_requires[i] = install_requires[i].strip()
+
+setuptools.setup(
+    name='test_data',
+    author='Zoltan Bozoky',
+    description='A script that generates 3 x 300 clinical metadata, pipeline metadata and corresponding variants for '
+                'testing purposes',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/CanDIG/test_data',
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    python_requires='>=3.7',
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'test_data=test_data.generate_vcf:main',
+        ],
+    },
+)
