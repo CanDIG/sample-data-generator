bfc0a1565cfa3765a931a6f2a2836613d6e2db39
Samarth Agarwal <agarwalsamarth@hotmail.com>
2020-07-22 17:59:35 -0400

    Refactored setup.py to take into account repo name has been changed to sample-data-generator.

--- setup.py
+++ setup.py
@@ -10,20 +10,20 @@ with open('requirements.txt', 'r') as file:
         install_requires[i] = install_requires[i].strip()
 
 setuptools.setup(
-    name='test_data',
+    name='sample_data_generator',
     author='Zoltan Bozoky',
     description='A script that generates 3 x 300 clinical metadata, pipeline metadata and corresponding variants for '
                 'testing purposes',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/CanDIG/test_data',
+    url='https://github.com/CanDIG/sample-data-generator',
     package_dir={'': 'src'},
     packages=setuptools.find_packages(where='src'),
     python_requires='>=3.7',
     install_requires=install_requires,
     entry_points={
         'console_scripts': [
-            'test_data=test_data.generate_vcf:main',
+            'generate_sample_data=sample_data_generator.generate_vcf:main',
         ],
     },
 )
